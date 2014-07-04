834d8b0c06ca9fc64eb6c3640f4f6870a228d592
Patrick Melia <patrickmelia82@gmail.com>
2014-07-04 18:23:37 +0100

    For Tómas
    For Tómas

---
+++ Blog/Blog/Controllers/Blog_Controller.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Blog.Models;
+
+namespace Blog.Controllers
+{
+    public class Blog_Controller : Controller
+    {
+        private UsersContext db = new UsersContext();
+
+        //
+        // GET: /Blog_/
+
+        public ActionResult Index()
+        {
+            return View(db.Blogs.ToList());
+        }
+
+        //
+        // GET: /Blog_/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Blogs blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        //
+        // GET: /Blog_/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Blog_/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Blogs blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blog);
+        }
+
+        //
+        // GET: /Blog_/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Blogs blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        //
+        // POST: /Blog_/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Blogs blog)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blog).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blog);
+        }
+
+        //
+        // GET: /Blog_/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Blogs blog = db.Blogs.Find(id);
+            if (blog == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blog);
+        }
+
+        //
+        // POST: /Blog_/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Blogs blog = db.Blogs.Find(id);
+            db.Blogs.Remove(blog);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Blog/Blog/Models/BlogEntites.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Blog.Models
+{
+    public class Blogs
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    [Table("Posts")]
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public virtual Blogs Blog { get; set; }
+    }
+}
